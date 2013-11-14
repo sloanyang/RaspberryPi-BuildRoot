bb3cf703385b5381e50cdae765a63f8adf77437a
yangqi3 <sloanyang@gmail.com>
2013-11-14 15:13:32 +0800

    Add package harfbuzz

---
+++ package/harfbuzz/harfbuzz.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# harfbuzz
+#
+################################################################################
+
+HARFBUZZ_VERSION = 0.9.5
+HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.bz2
+HARFBUZZ_SITE = http://www.freedesktop.org/software/harfbuzz/release
+HARFBUZZ_LICENSE = license
+HARFBUZZ_LICENSE_FILES = LICENSE
+HARFBUZZ_INSTALL_STAGING = YES
+
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
