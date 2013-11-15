a1e0d6a329a15f189aa2acd351d6b0689414eb06
yangqi3 <sloanyang@gmail.com>
2013-11-15 08:50:15 +0800

    Use custom libsecret to avoid man page generation error

--- package/libsecret/libsecret.mk
+++ package/libsecret/libsecret.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 LIBSECRET_VERSION = 0.11
-LIBSECRET_SOURCE = libsecret-$(LIBSECRET_VERSION).tar.xz
-LIBSECRET_SITE = http://ftp.gnome.org/pub/gnome/sources/libsecret/$(LIBSECRET_VERSION)
+LIBSECRET_SOURCE = libsecret-$(LIBSECRET_VERSION).tar.gz
+LIBSECRET_SITE = https://www.dropbox.com/s/gnlaw6d00s5p5g1
 LIBSECRET_LICENSE = license
 LIBSECRET_LICENSE_FILES = LICENSE
 LIBSECRET_INSTALL_STAGING = YES
