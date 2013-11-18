5ed13807cdb8045844a6e2c448d510bcba9c29a6
yangqi3 <sloanyang@gmail.com>
2013-11-18 09:35:15 +0800

    Fixing bugs on xslt

--- package/libxslt/libxslt.mk
+++ package/libxslt/libxslt.mk
@@ -18,9 +18,23 @@ LIBXSLT_DEPENDENCIES = libxml2
 # If we have enabled libgcrypt then use it, else disable crypto support.
 ifeq ($(BR2_PACKAGE_LIBGCRYPT),y)
 LIBXSLT_DEPENDENCIES += libgcrypt
-LIBXSLT_CONF_ENV += LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config
+LIBXSLT_CONF_ENV += LIBGCRYPT_CONFIG=$(STAGING_DIR)/usr/bin/libgcrypt-config \
+		PKG_CONFIG_DIR= \
+		PKG_CONFIG_LIBDIR=${STAGING_DIR}/usr/lib/pkgconfig:${STAGING_DIR}/usr/share/pkgconfig \
+		PKG_CONFIG_PATH=${STAGING_DIR}/usr/lib/pkgconfig:${STAGING_DIR}/usr/share/pkgconfig \
+		PKG_CONFIG_STAGING_DIR_DIR=${STAGING_DIR} \
+		LIBXML_LIBS=-L${STAGING_DIR}/usr/lib 
+
+LIBXSLT_CONF_OPT += --with-sysroot=$(STAGING_DIR)
 else
-LIBXSLT_CONF_OPT += --without-crypto
+LIBXSLT_CONF_ENV += \
+		PKG_CONFIG_DIR= \
+		PKG_CONFIG_LIBDIR=${STAGING_DIR}/usr/lib/pkgconfig:${STAGING_DIR}/usr/share/pkgconfig \
+		PKG_CONFIG_PATH=${STAGING_DIR}/usr/lib/pkgconfig:${STAGING_DIR}/usr/share/pkgconfig \
+		PKG_CONFIG_STAGING_DIR_DIR=${STAGING_DIR} \
+		LIBXML_LIBS=-L${STAGING_DIR}/usr/lib 
+
+LIBXSLT_CONF_OPT += --without-crypto --with-sysroot=$(STAGING_DIR)
 endif
 
 HOST_LIBXSLT_CONF_OPT = --without-debug --without-python --without-crypto
